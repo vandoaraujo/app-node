bdbcc97411b8779fbcdf6a66cc02375c1a98c2cc
Vando Araujo <tecladista.vando@gmail.com>
2025-12-23 18:09:55 -0300

    config

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -10,6 +10,7 @@ class LoginController {
   static async logar(req, res) {
     console.log("Entrou no login controller");
     console.log("Token carregado do .env:", TOKEN);
+    console.log(TOKEN);
 
     const options = {
       'method': 'POST',
